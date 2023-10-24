74d9f32e18ceedf5940e28cb4f057a1971b86cb6
softyAmir <theyabcpssouaf2@gmail.com>
2023-10-23 23:03:03 -1100

    .

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductServiceService } from '../product-service.service';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.css']
 })
-export class OverviewComponent {
+export class OverviewComponent implements OnInit {
+  public product: any;
+  constructor(private productService: ProductServiceService){}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(res=>{
+      this.product = res.data.length;
+    })
+  }
+
+  
 
 }
