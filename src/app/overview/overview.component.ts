5779a94f5bc463c099f9345dd21c71156e04f292
softyAmir <theyabcpssouaf2@gmail.com>
2023-11-07 10:11:14 -1100

    update overview

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductServiceService } from '../product-service.service';
+import { OrdersService } from '../orders.service';
 
 @Component({
   selector: 'app-overview',
@@ -8,15 +9,44 @@ import { ProductServiceService } from '../product-service.service';
 })
 export class OverviewComponent implements OnInit {
   public product: any;
-  
-  constructor(private productService: ProductServiceService){}
+  public orders: any;
+  public nbCostumer:number = 1;
+  public Totalamount:number = 0;
+  public nbOrders:number = 0;
+  constructor(
+    private productService: ProductServiceService,
+    private orderS:OrdersService
+    ){}
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe(res=>{
       this.product = res.data.length;
     })
+    this.orderS.getOrderById("6547ee2d542e6d53e008cef5").subscribe(
+      (res) => {
+        this.orders = res;
+        this.Totalamount = this.calculateTotalAmountWithStatusTrue(this.orders.orders);
+        this.nbOrders = this.getOrderLength(this.orders.orders);
+        
+      },
+      (error) => {
+        console.error('Error fetching orders:', error);
+      }
+    );
   }
 
+  public getOrderLength(orders: any[]): number {
+    return orders.length;
+  }
   
+  public calculateTotalAmountWithStatusTrue(orders: any[]): number {
+    let totalAmount = 0;
+    for (const order of orders) {
+      if (order.status === true) {
+        totalAmount += order.totalAmount;
+      }
+    }
+    return totalAmount;
+  }
 
 }
