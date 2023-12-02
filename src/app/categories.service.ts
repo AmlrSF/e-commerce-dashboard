8f62d8bc0f40f9cea804338cbd7af026054f77e1
softyAmir <theyabcpssouaf2@gmail.com>
2023-12-01 22:41:15 -1100

    categoeries crud service

--- src/app/categories.service.ts
+++ src/app/categories.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:3000/api/v1/categories';
+
+  constructor(private http: HttpClient) { }
+
+  getAllCategories(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}`);
+  }
+
+  getCategoryById(id: string): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/${id}`);
+  }
+
+  createCategory(data: any): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}`, data);
+  }
+
+  updateCategoryById(id: string, data: any): Observable<any> {
+    return this.http.put<any>(`${this.baseUrl}/${id}`, data);
+  }
+
+  deleteCategoryById(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.baseUrl}/${id}`);
+  }
+
+  deleteAllCategories(): Observable<any> {
+    return this.http.delete<any>(`${this.baseUrl}`);
+  }
+  
 }
